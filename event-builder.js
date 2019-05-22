e8fbc985525f49e22344319e387f5cf07ac93f8c
p15orellana <pedro15orellana1@gmail.com>
2019-05-22 17:58:17 -0500

    Updates

--- event-builder.js
+++ event-builder.js
@@ -5,102 +5,177 @@
 var events = [
   {
     title: "Settlers of Catan",
-    genre: "modern strategy",
-    complexity: 2.34,
-    min_players: 3,
-    max_players: 4,
-    playtime: 90,
+    genre: "Modern Strategy",
+    complexity: "2.34",
+    min_players: "3",
+    max_players: "4",
+    playtime: "90",
     date: "7/12/19",
     time: "5:00 PM",
-    street_address: "560 Foxrun CT",
+    street_address: "560 Foxrun Ct",
     city: "Evanston",
     state: "IL",
-    comment: "Bring Snacks"
+    comment: "Bring Snacks",
+    playstyle: "Competitive"
   },
   {
     title: "Azul",
     genre: "Abstract Strategy",
-    complexity: 1.8,
-    min_players: 2,
-    max_players: 4,
-    playtime: 45,
+    complexity: "1.8",
+    min_players: "2",
+    max_players: "4",
+    playtime: "45",
     date: "05/25/19",
     time: "7:30 PM",
-    street_address: "500 Pulaski rd",
+    street_address: "500 Pulaski Rd",
     city: "Chicago",
     state: "IL",
-    comment: "Get ready for a fun time"
+    comment: "Get ready for a fun time",
+    playstyle: "Casual"
   },
   {
     title: "BlackJack",
     genre: "Cards",
-    complexity: 1.51,
-    min_players: 2,
-    max_players: 10,
-    playtime: 30,
+    complexity: "1.51",
+    min_players: "2",
+    max_players: "10",
+    playtime: "30",
     date: "06/1/19",
     time: "10:00 PM",
-    street_address: "2107 lake st",
+    street_address: "2107 Lake St",
     city: "Evanston",
     state: "IL",
-    comment: ""
+    comment: "",
+    playstyle: "Casual"
   },
   {
     title: "Die Tavernen im Tiefen Thal",
     genre: "Dice",
-    complexity: 2.71,
-    min_players: 2,
-    max_players: 4,
-    playtime: 60,
+    complexity: "2.71",
+    min_players: "2",
+    max_players: "4",
+    playtime: "60",
     date: "08/25/19",
     time: "12:00 PM",
-    street_address: "1720 Melvina ave",
+    street_address: "1720 Melvina Ave",
     city: "Chicago",
     state: "IL",
-    comment: ""
+    comment: "",
+    playstyle: "Competitive"
   },
   {
     title: "Icaion",
     genre: "RPG",
-    complexity: "",
-    min_players: 2,
-    max_players: 4,
-    playtime: 120,
+    complexity: "4",
+    min_players: "2",
+    max_players: "4",
+    playtime: "120",
     date: "06/20/19",
     time: "11:00 AM",
-    street_address: "560 Lincoln st",
+    street_address: "560 Lincoln St",
     city: "Evanston",
     state: "IL",
-    comment: "Get ready for a long game"
+    comment: "Get ready for a long game",
+    playstyle: "Competitive"
   },
   {
     title: "Just One",
     genre: "Party",
     complexity: 1.17,
-    min_players: 3,
-    max_players: 7,
-    playtime: 20,
+    min_players: "3",
+    max_players: "7",
+    playtime: "20",
     date: "6/20/19",
     time: "2:20 PM",
-    street_address: "8720 Long st",
+    street_address: "8720 Long St",
     city: "Chicago",
     state: "IL",
-    comment: ""
+    comment: "",
+    playstyle: "Competitive"
   }
 ];
+
+// document.getElementsByTagName('select')[0].onchange=function(){
 
 // Fill in this function to dyamically construct the HTML elements for the album list
 function buildeventlist() {
   var eventList = document.getElementById("event-list");
+
+  //Clear the list
+  var child = eventList.firstChild;
+  while(child) {
+    eventList.removeChild(child);
+    child = eventList.firstChild;
+  }
+
+  // Get the values of the selectors
+  var category_selector = document.getElementById("category-selector");
+  var tryhard_selector = document.getElementById("tryhard-selector");
+  var player_selector = document.getElementById("player-selector");
+  var category = category_selector[category_selector.selectedIndex].text;
+  var tryhard = tryhard_selector[tryhard_selector.selectedIndex].text;
+  var player = player_selector[player_selector.selectedIndex].text;
+
   for (let event of events) {
+    // if the category doesn't match continue;
+    if(category !== "Select") {
+      if(category.toLowerCase() !== event.genre.toLowerCase()) {
+        continue;
+      }
+    }
+    // also track player count
+    if(player !== "Select") {
+      let to_continue = true;
+      if(player === "1-4") {
+        if(event.max_players < 5) {
+          to_continue = false;
+        }
+      }
+      else if(player === "5-10") {
+        if(event.max_players < 11 && event.max_players > 4) {
+          to_continue = false;
+        }
+      }
+      else if(player === "10+") {
+        if(event.max_players > 10) {
+          to_continue = false;
+        }
+      }
+      if(to_continue) {
+        continue;
+      }
+    }
+    // Also track playstyle
+    if(tryhard !== "Select" && tryhard.toLowerCase() !== event.playstyle.toLowerCase()) {
+      continue;
+    }
+
+   
+
     var eventDiv = document.createElement('div');
     let child = '';
     eventDiv.classList.add('event-wrapper');
-    child += `<p class="event-title">Title: ${event.title}</p>`;
-    child += `<p class="event-address">Address: ${event.street_address}</p>`;
-    child += `<p class="event-city"> City: ${event.city}</p>`;
-    child += `<p class="event-date">Date: ${event.date}</p>`;
-    child += `<p class="event-time">Time: ${event.time}</p>`;
+    child += `
+    <div class="game-div">
+    <p class="event-title">${event.title}</p>
+    <p class="event-address">${event.genre}</p>
+    <p class="event-city">${event.max_players} players</p>
+    <p class="event-date">${event.playstyle}</p>
+    <p class="event-time">${event.playtime} min</p>
+    </div>
+    <div class="game-div">
+    <p class="event-title">${event.date}</p>
+    <p class="event-time">${event.time}</p>
+    <p class="event-address">${event.street_address}</p>
+    <p class="event-city"> ${event.city}</p>
+    <p class="event-date">${event.state}</p>
+    
+    </div>
+    <form action="./index.html">
+    <button>Join</button>
+    </form>
+    `;
+  
     eventDiv.innerHTML = child;
     if (eventList != null) {
       eventList.appendChild(eventDiv);
